08443d354f42cd2d6e84529b31b21a646d89383f
Kevin Kuchta <kevin@kevinkuchta.com>
2018-08-08 11:38:37 -0700

    Use title as seed when provided

--- markov_function/produce_post.py
+++ markov_function/produce_post.py
@@ -1,21 +1,26 @@
 import json
 import random
+import base64
 from markov_function.posified_text import *
 
 def produce(event, context):
+    print(event)
+    title = None
     if 'queryStringParameters' in event and event['queryStringParameters'] != None:
-        seed = event['queryStringParameters']['seed']
-        random.seed(seed)
-        print(seed)
+        title64 = event['queryStringParameters']['title64']
+        title = base64.b64decode(title64).decode("utf-8")
 
-    with open('markov_function/title_chain.json') as f:
-        title_data = f.read()
+    else:
+        with open('markov_function/title_chain.json') as f:
+            title_data = f.read()
+        title_model = POSifiedText.from_json(title_data)
+        title = title_model.make_short_sentence(240)
+
+    random.seed(title)
     with open('markov_function/paragraphs_chain.json') as f:
         paragraphs_data = f.read()
 
-    title_model = POSifiedText.from_json(title_data)
     paragraphs_model = POSifiedText.from_json(paragraphs_data)
-
     paragraphs = paragraphs_model.make_sentence()
 
     # For some reason double periods keep showing up.  Remove them.
@@ -23,8 +28,8 @@ def produce(event, context):
 
     # TODO: make paragraph sentences up to a certain max length
     output = {
-        'title': title_model.make_short_sentence(240),
-        'paragraphs': paragraphs_model.make_sentence()
+        'title': title,
+        'paragraphs': paragraphs
     }
 
     #print(json.dumps(output))
